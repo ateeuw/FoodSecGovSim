489924f957a71ea5ad336a4a09dd93d862f1e721
Teeuwen <a.s.teeuwen@utwente.nl>
2021-11-17 17:01:07 +0100

    added some dependencies

--- Full_analysis.R
+++ Full_analysis.R
@@ -39,7 +39,9 @@ source("./Data_for_MFA.R")
 rm(list = ls()) #start with a clean environment
 
 # Install dependencies
-list.of.packages <- c("dplyr", "tidyr", "FactoMineR", "PCAmixdata", "ggforce", "ggplot2", "ggalt", "concaveman", "stringi")
+list.of.packages <- c("dplyr", "tidyr", "FactoMineR", "PCAmixdata", "ggforce", "ggplot2",
+                      "ggalt", "concaveman", "stringi", "Rfast", "kableExtra", "webshot",
+                      "stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
